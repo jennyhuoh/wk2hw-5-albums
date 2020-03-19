2d11628c9d3babc58ad1d277bb930743879598c1
jennyhuoh <huoh.li@gmail.com>
2020-03-19 17:55:37 +0800

    done

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
-import Header from './src/components/Header';
-import AlbumList from './src/components/AlbumList';
+import Header from './components/Header';
+import AlbumList from './components/AlbumList';
 
 const App = () => {
   return ( 
     <View>
       <Header />
       <ScrollView>
-       
+
         <AlbumList />
       </ScrollView>
     </View>
